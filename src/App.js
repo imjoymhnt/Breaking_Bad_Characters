a3d1c4c0a82bd37f37e770bbbb6de9c3b4986bb3
Mritunjoy Mahanta <mritunjoy.mahanta001@gmail.com>
2021-06-08 15:11:37 +0530

    breaking bad api data fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
 import './App.css';
+import Header from './components/ui/Header';
+import axios from 'axios'
+import CharacterGrid from './components/characters/CharacterGrid';
 
 function App() {
+
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const {data} = await axios.get('https://www.breakingbadapi.com/api/characters')
+      console.log(data);
+      setItems(data)
+      setIsLoading(false)
+    }
+    fetchItems()
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 }
